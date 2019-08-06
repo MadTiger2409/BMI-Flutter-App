0668bc81cf5b8530e28c5fbb9adff5774ee18c6f
Michal B <cyberwolf_96@outlook.com>
2019-08-06 11:47:57 +0200

    Used Table widget to re-design resoults list/table

--- flutter_app/lib/pages/home/myHomePageState.dart
+++ flutter_app/lib/pages/home/myHomePageState.dart
@@ -83,9 +83,9 @@ class MyHomePageState extends State<MyHomePage> {
             ),
           ),
           floatingActionButton: FloatingActionButton(
-            onPressed: (){
+            onPressed: () {
               _settingModalBottomSheet(context);
-              },
+            },
             tooltip: 'Info',
             child: Icon(Icons.info_outline),
           ),
@@ -96,57 +96,92 @@ class MyHomePageState extends State<MyHomePage> {
 
   void _settingModalBottomSheet(context) {
     showModalBottomSheet(
-      context: context,
-      builder: (BuildContext bc) {
-        return Container(
-          padding: EdgeInsets.all(30.00),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: _createResultsRows(_createResultsList()),
-          ),
-        );
-      }
-    );
-  }
-
-  List<Widget> _createResultsRows(List<Result> results) {
-    List<Widget> resultRows = List<Widget>();
-
-    results.forEach((f) => resultRows.add(
-      Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(right: 20.00),
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            padding: EdgeInsets.all(30.00),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Text(
-                  '${f.bmiLevel}',
+                Center(
+                  child: Table(
+                    columnWidths: {
+                      1: FlexColumnWidth(2),
+                    },
+                    children: _createResultsRows(_createResultsList()),
+                  ),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  List<TableRow> _createResultsRows(List<Result> results) {
+    List<TableRow> resultRows = List<TableRow>();
+
+    resultRows.add(
+      TableRow(
+        children: <Widget>[
+          TableCell(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Result',
+                style: TextStyle(
+                  fontSize: 22.00,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          TableCell(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Description',
+                style: TextStyle(
+                  fontSize: 22.00,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    results.forEach(
+      (f) => resultRows.add(
+        TableRow(
+          children: <Widget>[
+            TableCell(
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  f.bmiLevel,
                   style: TextStyle(
                     color: f.levelTextColor,
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                '${f.bmiLevelDescription}',
-                style: TextStyle(
-                  color: f.levelTextColor,
+            TableCell(
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  f.bmiLevelDescription,
+                  style: TextStyle(
+                    color: f.levelTextColor,
+                  ),
                 ),
               ),
-            ],
-          )
-        ],
-      ))
+            ),
+          ],
+        ),
+      ),
     );
 
     return resultRows;
@@ -155,17 +190,28 @@ class MyHomePageState extends State<MyHomePage> {
   List<Result> _createResultsList() {
     List<Result> results = List<Result>();
 
-    results.add(Result('< 16.0:', 'Very severely underweight', Color.fromRGBO(0, 85, 255, 1.00)));
-    results.add(Result('16.00–16.99:', 'Severely underweight', Color.fromRGBO(0, 183, 255, 1.00)));
-    results.add(Result('17.00–18.49:', 'Underweight', Color.fromRGBO(0, 224, 198, 1.00)));
-    results.add(Result('18.50–24.99:', 'Normal', Color.fromRGBO(0, 191, 6, 1.00)));
-    results.add(Result('25.00–29.99:', 'Overweight', Color.fromRGBO(255, 213, 0, 1.00)));
-    results.add(Result('30.00–34.99:', 'Obese Class I (Moderately obese)', Color.fromRGBO(255, 174, 0, 1.00)));
-    results.add(Result('35.00–39.99:', 'Obese Class II (Severely obese)', Color.fromRGBO(255, 132, 0, 1.00)));
-    results.add(Result('40.00-44.99:', 'Obese Class III (Very severely obese)', Color.fromRGBO(255, 106, 0, 1.0)));
-    results.add(Result('45.00-49.99:', 'Obese Class IV (Morbidly Obese)', Color.fromRGBO(0, 85, 255, 1.00)));
-    results.add(Result('50.00-59.99:', 'Obese Class V (Super Obese)', Color.fromRGBO(255, 10, 0, 1.0)));
-    results.add(Result('≥ 60.00:', 'Obese Class VI (Hyper Obese)', Color.fromRGBO(150, 52, 23, 1.0)));
+    results.add(Result('< 16.0:', 'Very severely underweight',
+        Color.fromRGBO(0, 85, 255, 1.00)));
+    results.add(Result('16.00–16.99:', 'Severely underweight',
+        Color.fromRGBO(0, 183, 255, 1.00)));
+    results.add(Result(
+        '17.00–18.49:', 'Underweight', Color.fromRGBO(0, 224, 198, 1.00)));
+    results
+        .add(Result('18.50–24.99:', 'Normal', Color.fromRGBO(0, 191, 6, 1.00)));
+    results.add(Result(
+        '25.00–29.99:', 'Overweight', Color.fromRGBO(255, 213, 0, 1.00)));
+    results.add(Result('30.00–34.99:', 'Obese Class I (Moderately obese)',
+        Color.fromRGBO(255, 174, 0, 1.00)));
+    results.add(Result('35.00–39.99:', 'Obese Class II (Severely obese)',
+        Color.fromRGBO(255, 132, 0, 1.00)));
+    results.add(Result('40.00-44.99:', 'Obese Class III (Very severely obese)',
+        Color.fromRGBO(255, 106, 0, 1.0)));
+    results.add(Result('45.00-49.99:', 'Obese Class IV (Morbidly Obese)',
+        Color.fromRGBO(0, 85, 255, 1.00)));
+    results.add(Result('50.00-59.99:', 'Obese Class V (Super Obese)',
+        Color.fromRGBO(255, 10, 0, 1.0)));
+    results.add(Result('≥ 60.00:', 'Obese Class VI (Hyper Obese)',
+        Color.fromRGBO(150, 52, 23, 1.0)));
 
     return results;
   }
