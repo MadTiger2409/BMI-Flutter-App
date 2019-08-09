d5548f9bd1189d97644b9e70771a3c0e72012204
Michał B <cyberwolf_96@outlook.com>
2019-08-09 12:05:37 +0200

    Changed app name

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fit App',
+      title: 'BMI Calc',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
